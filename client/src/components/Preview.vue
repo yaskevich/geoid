1ae468d5f9f9c32cf2fafeee69f91fd4fda30e2c
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2024-04-20 22:40:40 +0200

    create component

---
+++ client/src/components/Preview.vue
@@ -0,0 +1,20 @@
+<template>
+    <n-space>
+        <n-tooltip trigger="hover"
+            v-for="(val) in Object.entries(data).filter(x => x?.[1] && !['id', 'place_be', 'place_ru', 'lon', 'lat'].includes(x[0])) ">
+            <template #trigger><n-tag>{{ val[1] }}</n-tag></template>
+            {{ val[0] }}
+        </n-tooltip>
+    </n-space>
+</template>
+
+<script setup lang="ts">
+
+import { NTag, NSpace, NIcon } from 'naive-ui';
+
+defineProps<{
+    data: keyable,
+}>();
+
+
+</script>
